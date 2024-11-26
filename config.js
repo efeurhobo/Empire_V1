f5059c7b03f3412eb6ca3ab10186f56e74ea183c
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-11-26 09:51:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,4 +12,5 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/g4qzs7.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR EMPIRE_V1 IS ONLINE",
 PREFIX: process.env.PREFIX || ".",
 OWNER_REACT: process.env.OWNER_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 };
