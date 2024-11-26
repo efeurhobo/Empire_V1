0ea043979cd6c7e4f09f47d3a2ddcb59e7dc02c2
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-11-26 07:06:19 +0100

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -1,1 +1,39 @@
+const { cmd, commands } = require('../command');
+const { readEnv } = require('../lib/database');
 
+cmd({
+    pattern: "tagall",
+    desc: "Tags every person in the group.",
+    category: "group",
+    filename: __filename,
+}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber, pushname, groupMetadata, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) return reply("This command can only be used in a group.");
+        
+        // Fetch group metadata to get participants
+        groupMetadata = await conn.groupMetadata(from);
+        participants = groupMetadata.participants;
+
+        let textt = `
+◐╤╤✪〘 *Tag All* 〙✪╤╤◑
+
+➲ *Message:* ${args.join(' ') || "blank"}\n\n
+➲ *Author:* ${pushname} 💀
+        `;
+        
+        // Loop through participants and tag each member
+        for (let mem of participants) {
+            textt += `👤 @${mem.id.split('@')[0]}\n`;
+        }
+
+        // Send the tagged message
+        await conn.sendMessage(from, {
+            text: textt,
+            mentions: participants.map(a => a.id),
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply("An error occurred while trying to tag all members.");
+    }
+});
