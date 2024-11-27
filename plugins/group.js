44fccd7f061370dae6a85b400dee396b7aacb34c
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-11-27 02:02:15 +0100

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -170,4 +170,56 @@ try{
         console.error('Error tagging admins:', e);
         reply('you are not an admin.');
     }
+});
+
+//mute and unmute 
+
+cmd({
+    pattern: "mute",	
+    alias: ["lock"],
+    react: "🔒",
+    desc: "mute group.",
+    category: "group",
+    filename: __filename,
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    
+if (!isOwner || !isAdmins) return;
+
+
+if (!m.isGroup) return reply(mg.onlygroup);
+if (!isBotAdmins) return reply(mg.needbotadmins);     
+            await conn.groupSettingUpdate(m.chat, "announcement")
+           const mass = await conn.sendMessage(m.chat, { text: '*GROUP CHAT MUTED BY 𝐄𝐦𝐩𝐢𝐫𝐞_𝐕𝟏* 🔒' }, { quoted: mek });
+            return await conn.sendMessage(m.chat, { react: { text: '🔒', key: mass.key } });
+} catch(e) {
+console.log(e);
+reply('*PLEASE GIVE ME A ADDMIN ROLE❗👻*')    
+} 
 })
+
+cmd({
+    pattern: "unmute",	
+    alias: ["unlock"],
+    react: "🔓",
+    desc: "unmute group.",
+    category: "group",
+    filename: __filename,
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    
+if (!isOwner || !isAdmins) return;
+
+
+if (!m.isGroup) return reply(mg.onlygroup);
+if (!isBotAdmins) return reply(mg.needbotadmins);     
+            await conn.groupSettingUpdate(m.chat, "not_announcement")
+           const mass = await conn.sendMessage(m.chat, { text: '*GROUP CHAT UNMUTED BY SILENT-𝐄𝐦𝐩𝐢𝐫𝐞_𝐕𝟏* 🔒' }, { quoted: mek });
+            return await conn.sendMessage(m.chat, { react: { text: '🔒', key: mass.key } });
+} catch(e) {
+console.log(e);
+reply('*PLEASE GIVE ME A ADDMIN ROLE❗👻*')    
+} 
+});
