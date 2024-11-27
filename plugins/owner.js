0d79e2b235d5f7f46d8ba77bd916c27537cbb8eb
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-11-27 01:09:48 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -118,3 +118,30 @@ cmd({
         reply(`❌ Error clearing chats: ${error.message}`);
     }
 });
+
+// DELETE COMMANDS
+cmd({
+pattern: "delete",
+react: "❌",
+alias: ["del"],
+desc: "delete message",
+category: "owner",
+use: '.del',
+filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+if (!isOwner ||  !isAdmins) return;
+try{
+if (!m.quoted) return reply(mg.notextfordel);
+const key = {
+            remoteJid: m.chat,
+            fromMe: false,
+            id: m.quoted.id,
+            participant: m.quoted.sender
+        }
+        await conn.sendMessage(m.chat, { delete: key })
+} catch(e) {
+console.log(e);
+reply('successful..👨‍💻✅')
+} 
+});
